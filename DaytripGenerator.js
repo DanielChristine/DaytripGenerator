ab635fd416750faff9d18c78fa918fe86d2894eb
Daniel Christine <danieljameschristine@gmail.com>
2021-01-18 19:15:10 -0600

    first 10 minutes

---
+++ DaytripGenerator.js
@@ -0,0 +1,10 @@
+var destination = ['Spain', 'Geneva', 'Florida', 'Local'];
+var restaurant = ['Italian', 'Greek', 'French', 'American'];
+var transportation = ['Bus', 'Bike', 'Plane', 'Pogostick'];
+var entertainment = ['Movie', 'Hiking', 'Volunteer', 'Be Weird'];
+
+function randomize() {
+    return Math.floor(Math.random() * 4);
+    
+  }
+randomize();
