27a5219319b1c384b01897dc33c1a5907c5fdb1b
Daniel Christine <danieljameschristine@gmail.com>
2021-01-18 19:33:01 -0600

    Added randomizer for destination.

--- DaytripGenerator.js
+++ DaytripGenerator.js
@@ -1,10 +1,26 @@
-var destination = ['Spain', 'Geneva', 'Florida', 'Local'];
-var restaurant = ['Italian', 'Greek', 'French', 'American'];
-var transportation = ['Bus', 'Bike', 'Plane', 'Pogostick'];
-var entertainment = ['Movie', 'Hiking', 'Volunteer', 'Be Weird'];
+var destination = ["Spain", "Geneva", "Florida", "Local"];
+var restaurant = ["Italian", "Greek", "French", "American"];
+var transportation = ["Bus", "Bike", "Plane", "Pogostick"];
+var entertainment = ["Movie", "Hiking", "Volunteer", "Be Weird"];
+
+var foundDestination = [];
+var foundRestauraunt = [];
+var foundTransportation = [];
+var foundEntertainment = [];
+
+var dayTrip = [];
 
 function randomize() {
-    return Math.floor(Math.random() * 4);
-    
-  }
-randomize();
+  return Math.floor(Math.random() * 4);
+}
+
+function randomDestination() {
+  var findDestination = destination[randomize()];
+
+  foundDestination.push(findDestination);
+  dayTrip.push(findDestination);
+  return findDestination;
+}
+randomDestination();
+
+console.log(foundDestination);
