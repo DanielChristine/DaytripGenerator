7143c95491bc0ef83f81a141b5228b5bf6c5f85f
Daniel Christine <danieljameschristine@gmail.com>
2021-01-18 20:25:18 -0600

    Added button to reroll options. Rerolling isn't properly clearing the array. All other function working.

--- DaytripGenerator.js
+++ DaytripGenerator.js
@@ -1,14 +1,12 @@
-var destination = ["Spain", "Geneva", "Florida", "Local"];
+var destination = ["Los Angelas", "Houston", "Miami", "Local"];
 var restaurant = ["Italian", "Greek", "French", "American"];
-var transportation = ["Bus", "Bike", "Plane", "Pogostick"];
-var entertainment = ["Movie", "Hiking", "Volunteer", "Be Weird"];
-
+var transportation = ["bus", "bike", "plane", "pogostick"];
+var entertainment = ["finger-painting", "hiking", "volunteering", "kite flying"];
+var dayTrip = [];
 var foundDestination = [];
-var foundRestauraunt = [];
+var foundRestaurant = [];
 var foundTransportation = [];
 var foundEntertainment = [];
-
-var dayTrip = [];
 
 function randomize() {
   return Math.floor(Math.random() * 4);
@@ -19,8 +17,68 @@ function randomDestination() {
 
   foundDestination.push(findDestination);
   dayTrip.push(findDestination);
+
   return findDestination;
 }
-randomDestination();
 
-console.log(foundDestination);
+function randomEntertainment() {
+  var findEntertainment = entertainment[randomize()];
+
+  foundEntertainment.push(findEntertainment);
+  dayTrip.push(findEntertainment);
+
+  return findEntertainment;
+}
+
+function randomTransportation() {
+  var findTransportation = transportation[randomize()];
+
+  foundTransportation.push(findTransportation);
+  dayTrip.push(findTransportation);
+
+  return findTransportation;
+}
+
+function randomRestaurant() {
+  var findRestaurant = restaurant[randomize()];
+
+  foundRestaurant.push(findRestaurant);
+  dayTrip.push(findRestaurant);
+
+  return findRestaurant;
+}
+
+console.log(dayTrip);
+
+function roll() {
+  dayTrip.length = 0;
+  randomDestination();
+  randomEntertainment();
+  randomTransportation();
+  randomRestaurant();
+  finalMessage();
+}
+
+function finalMessage() {
+  console.log(
+    "The suggested trip is to go to " +
+      foundDestination +
+      " Via " +
+      foundTransportation +
+      " eat " +
+      foundRestaurant +
+      " food. Then try your hand at " +
+      foundEntertainment
+  );
+}
+function planTrip(){
+
+randomDestination();
+randomEntertainment();
+randomTransportation();
+randomRestaurant();
+finalMessage();
+
+}
+
+planTrip();
